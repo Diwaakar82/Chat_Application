aa16c7ca9e4bc3a725d396bfaedd2e88668c7f4f
Diwaakar82 <diwaakar@mobicip.com>
2024-02-05 12:34:48 +0530

    Basic code

---
+++ client.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+
+#define PORT "8080"			//Port used for connections
+#define MAXDATASIZE 1000
+
+//Return the IPv4 or IPv6 address
+void *get_in_addr (struct sockaddr *sa)
+{
+	if (sa -> sa_family == AF_INET)
+		return &(((struct sockaddr_in *)sa) -> sin_addr);
+		
+	return &(((struct sockaddr_in6 *)sa) -> sin6_addr);
+}
+
+//Connect to a socket
+struct addrinfo *connect_to_socket (int *sockfd, struct addrinfo *servinfo)
+{
+	struct addrinfo *p;
+	
+	for (p = servinfo; p != NULL; p = p -> ai_next)
+	{
+		//Socket not free
+		if ((*sockfd = socket (p -> ai_family, p -> ai_socktype, p -> ai_protocol)) == -1)
+		{
+			perror ("client: socket");
+			continue;
+		}
+		//Connect to socket
+		if (connect (*sockfd, p -> ai_addr, p -> ai_addrlen) == -1)
+		{
+			close (*sockfd);
+			perror ("client: connect");
+			continue;
+		}
+		break;
+	}
+	
+	return p;
+}
+
+//Initialize address information
+void initialize (struct addrinfo *hints)
+{
+	memset (hints, 0, sizeof *hints);
+	
+	hints -> ai_family = AF_UNSPEC;
+	hints -> ai_socktype = SOCK_STREAM;
+	hints -> ai_flags = AI_PASSIVE;
+}
+
+//Send message
+void send_message (int sockfd, char *msg)
+{
+	if (send (sockfd, msg, sizeof (msg), 0) == -1)
+	{
+		perror ("send");
+		exit (0);
+	}
+}
+
+//Receive message
+int receive_message (int sockfd, char *buf)
+{
+	int numbytes;
+	if ((numbytes = recv (sockfd, buf, 1000, 0)) == -1)
+	{
+		perror ("recv");
+		exit (0);
+	}
+	return numbytes;
+}
+
+int main (int argc, char *argv [])
+{
+	int sockfd, numbytes;
+	char buf [MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s [INET6_ADDRSTRLEN];
+	
+	//Hostname not provided
+	if (argc != 2)
+	{
+		fprintf (stderr, "usage: client hostname\n");
+		exit (1);
+	}
+	
+	initialize (&hints);
+	
+	//Get server address
+	if ((rv = getaddrinfo (argv [1], PORT, &hints, &servinfo)) != 0)
+	{
+		fprintf (stderr, "getaddrinfo: %s\n", gai_strerror (rv));
+		return 1;
+	}
+	
+	p = connect_to_socket (&sockfd, servinfo);
+	
+	if (p == NULL)
+	{
+		fprintf (stderr, "client: failed to connect\n");
+		return 2;
+	}
+	
+	inet_ntop (p -> ai_family, get_in_addr ((struct sockaddr *) p -> ai_addr), s, sizeof s);
+	printf ("client: connecting to %s\n", s);
+	
+	//Server information not required anymore
+	freeaddrinfo (servinfo);
+	
+	send_message (sockfd, "Hi");	
+	numbytes = receive_message (sockfd, buf);
+
+	printf ("Client received: %s\n", buf);
+	close (sockfd);
+	
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+
+#define PORT "8080"			//Port used for connections
+#define BACKLOG 2			//Pending connections queue can hold
+
+//Fetch internet address
+void *get_in_addr (struct sockaddr *sa)
+{
+	if (sa -> sa_family == AF_INET)
+		return &(((struct sockaddr_in *) sa) -> sin_addr);
+		
+	return &(((struct sockaddr_in6 *) sa) -> sin6_addr);
+}
+
+//Connect to a socket
+struct addrinfo *connect_to_socket (int *sockfd, struct addrinfo *servinfo)
+{
+	struct addrinfo *p;
+	int yes = 1;
+	
+	for (p = servinfo; p != NULL; p = p -> ai_next)
+	{   
+		if ((*sockfd = socket (p -> ai_family, p -> ai_socktype, p -> ai_protocol)) == -1)
+		{
+			perror ("server: socket");
+			continue;
+		}
+		if (setsockopt (*sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof (int)) == -1)
+		{
+			perror ("setsockopt");
+			exit (1);
+		}
+		if (bind (*sockfd, p -> ai_addr, p -> ai_addrlen) == -1)
+		{
+			close (*sockfd);
+			perror ("server: bind");
+			continue;
+		}
+	}
+	
+	return p;
+}
+
+//Initialize address information
+void initialize (struct addrinfo *hints)
+{
+	memset (hints, 0, sizeof *hints);
+	
+	hints -> ai_family = AF_UNSPEC;
+	hints -> ai_socktype = SOCK_STREAM;
+	hints -> ai_flags = INADDR_ANY;
+}
+
+//Send message
+void send_message (int sockfd, char *msg)
+{
+	if (send (sockfd, msg, 1000, 0) == -1)
+	{
+		perror ("send");
+		exit (0);
+	}
+}
+
+//Receive message
+int receive_message (int sockfd, char *buf)
+{
+	int numbytes;
+	memset (buf, 0, 1000);
+	
+	if ((numbytes = recv (sockfd, buf, 1000, 0)) == -1)
+		perror ("recv");
+
+	return numbytes;
+}
+
+int main ()
+{
+	int sockfd, new_fd;
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr;
+	socklen_t sin_size;
+	char s [INET6_ADDRSTRLEN], IP [INET6_ADDRSTRLEN];
+	int rv;
+
+	initialize (&hints);
+	
+	//Get server address
+	if ((rv = getaddrinfo (NULL, PORT, &hints, &servinfo)) != 0)
+	{
+		fprintf (stderr, "getaddrinfo: %s\n", gai_strerror (rv));
+		return 1;
+	}
+	
+	p = connect_to_socket (&sockfd, servinfo);
+	
+	freeaddrinfo (servinfo);
+
+	//Failed to listen
+	if (listen (sockfd, BACKLOG) == -1)
+	{
+		perror ("listen");
+		exit (1);
+	}
+	
+	printf ("Server: waiting for connection: \n");
+
+	while (1)
+	{
+		sin_size = sizeof their_addr;
+        char buffer [1000], buff [1000];
+        
+		new_fd = accept (sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1)
+		{
+			perror ("accept");
+			continue;
+		}
+		
+		//Find server IP address
+		inet_ntop (their_addr.ss_family, get_in_addr ((struct sockaddr *)&their_addr), s, sizeof s);
+		printf ("Server: got connection from %s\n", s);
+		
+		//Read incoming message
+		receive_message (new_fd, buffer);
+		printf ("Messsage: %s\n", buffer);
+		
+		//Modify received message
+		memset (buff, '\0', 1000);	
+		
+		//sleep (5);
+		
+		//Send message to client
+		printf ("Buffer: %s\n", buffer);
+		send_message (new_fd, buffer);
+		memset (buffer, '\0', 1000);
+		
+		close (new_fd);
+	}
+	close (sockfd);
+	
+	return 0;
+}
